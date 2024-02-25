bb45b0b4a765b0aca33f9e3b71ca5574c8f36605
teixeira308 <teixeira308@hotmail.com>
2024-02-25 17:34:55 -0300

    ponto atual

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-
-
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import { AiOutlineHome, AiOutlineBulb, AiOutlineBook, AiOutlineMail } from 'react-icons/ai';
-import Modal from './components/Modal'; // Importe o componente Modal
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import {
+  AiOutlineHome,
+  AiOutlineBulb,
+  AiOutlineBook,
+  AiOutlineMail,
+} from "react-icons/ai";
+import Modal from "./components/Modal"; // Importe o componente Modal
 
 function App() {
+  
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [pokemonList, setPokemonList] = useState([]);
   const [pageSize, setPageSize] = useState(10);
-  const [viewMode, setViewMode] = useState('cards'); // Estado para controlar o modo de visualização
-
+  const [viewMode, setViewMode] = useState("cards"); // Estado para controlar o modo de visualização
 
   const [selectedPokemon, setSelectedPokemon] = useState(null); // Estado para armazenar o Pokémon selecionado
 
@@ -22,12 +24,14 @@ function App() {
   // Função para buscar a lista de Pokémon
   const fetchPokemonList = async (page, pageSize) => {
     try {
-      const response = await axios.get(`http://localhost:8000/pokemon?page=${page}&pageSize=${pageSize}`);
+      const response = await axios.get(
+        `http://localhost:8000/pokemon?page=${page}&pageSize=${pageSize}`
+      );
       setPokemonList(response.data);
       setCurrentPage(page);
-      setTotalPages(Math.ceil(response.headers['x-total-count'] / pageSize));
+      setTotalPages(Math.ceil(response.headers["x-total-count"] / pageSize));
     } catch (error) {
-      console.error('Erro ao obter a lista de Pokémon:', error);
+      console.error("Erro ao obter a lista de Pokémon:", error);
     }
   };
 
@@ -36,15 +40,14 @@ function App() {
     fetchPokemonList(1, pageSize);
   }, [pageSize]);
 
-
   // Componente para renderizar um Pokémon como card
   const PokemonCard = ({ pokemon }) => (
     <div className="card" key={pokemon.id}>
       <div className="id">ID: {pokemon.id}</div>
+      
       <div className="name">{pokemon.name}</div>
       <div className="createdAt">Data de Criação: {pokemon.createdAt}</div>
       <div className="createdAt">Data de Atualização: {pokemon.updatedAt}</div>
-      <button onClick={() => handleDeletePokemon(pokemon.id)}>Excluir</button>
       <button onClick={() => handleOpenModal(pokemon)}>Ver Detalhes</button>
     </div>
   );
@@ -56,7 +59,10 @@ function App() {
       <td>{pokemon.name}</td>
       <td>{pokemon.createdAt}</td>
       <td>{pokemon.updatedAt}</td>
-      <td> <button onClick={() => handleDeletePokemon(pokemon.id)}>Excluir</button> <button onClick={() => handleOpenModal(pokemon)}>Ver Detalhes</button></td>
+      <td>
+       
+        <button onClick={() => handleOpenModal(pokemon)}>Ver Detalhes</button>
+      </td>
     </tr>
   );
 
@@ -72,18 +78,17 @@ function App() {
     </thead>
   );
 
+  // Manipulador de evento para abrir o modal e definir o Pokémon selecionado
+  const handleOpenModal = (pokemon) => {
+    setSelectedPokemon(pokemon);
+    setIsModalOpen(true);
+  };
 
-    // Manipulador de evento para abrir o modal e definir o Pokémon selecionado
-    const handleOpenModal = (pokemon) => {
-      setSelectedPokemon(pokemon);
-      setIsModalOpen(true);
-    };
-  
-    // Manipulador de evento para fechar o modal
-    const handleCloseModal = () => {
-      setSelectedPokemon(null);
-      setIsModalOpen(false);
-    };
+  // Manipulador de evento para fechar o modal
+  const handleCloseModal = () => {
+    setSelectedPokemon(null);
+    setIsModalOpen(false);
+  };
 
   const handleViewDetails = (pokemon) => {
     // Aqui você pode implementar o redirecionamento para a página de detalhes do Pokémon
@@ -94,9 +99,12 @@ function App() {
     try {
       await axios.delete(`http://localhost:8000/pokemon/${id}`);
       // Após a exclusão bem-sucedida, atualiza a lista de Pokémon removendo o Pokémon excluído
-      setPokemonList(prevList => prevList.filter(pokemon => pokemon.id !== id));
+      setPokemonList((prevList) =>
+        prevList.filter((pokemon) => pokemon.id !== id)
+      );
+      handleCloseModal(); // Fecha o modal após a exclusão
     } catch (error) {
-      console.error('Erro ao excluir Pokémon:', error);
+      console.error("Erro ao excluir Pokémon:", error);
     }
   };
 
@@ -110,45 +118,61 @@ function App() {
     fetchPokemonList(1, pageSize);
   };
 
+  const updatePokemon = (updatedPokemon) => {
+    setSelectedPokemon(updatedPokemon);
+  };
+
   const handleLastPage = () => {
     fetchPokemonList(totalPages, pageSize);
   };
 
-
   // Função para alternar entre a visualização de cards e a visualização de itens em lista
   const toggleViewMode = () => {
-    setViewMode(prevMode => prevMode === 'cards' ? 'list' : 'cards'); // Alterna entre 'cards' e 'list'
+    setViewMode((prevMode) => (prevMode === "cards" ? "list" : "cards")); // Alterna entre 'cards' e 'list'
   };
 
   return (
     <div className="app-container">
-      
       <div className="menu">
-        <div className="menu" >
+        <div className="menu">
           {/* Botão de menu para dispositivos móveis */}
           {/* Conteúdo do menu */}
           <ul>
             <li>
               <a href="#">
-                <i className="fas fa-home">  <AiOutlineHome /></i> {/* Ícone para o link */}
+                <i className="fas fa-home">
+                  {" "}
+                  <AiOutlineHome />
+                </i>{" "}
+                {/* Ícone para o link */}
                 <span>Home</span> {/* Descrição do link */}
               </a>
             </li>
             <li>
               <a href="#">
-                <i className="fas fa-info-circle"><AiOutlineBulb /></i> {/* Ícone para o link */}
+                <i className="fas fa-info-circle">
+                  <AiOutlineBulb />
+                </i>{" "}
+                {/* Ícone para o link */}
                 <span>Sobre</span> {/* Descrição do link */}
               </a>
             </li>
             <li>
               <a href="#">
-                <i className="fas fa-paw"><AiOutlineBook /></i> {/* Ícone para o link */}
+                <i className="fas fa-paw">
+                  <AiOutlineBook />
+                </i>{" "}
+                {/* Ícone para o link */}
                 <span>Pokemons</span> {/* Descrição do link */}
               </a>
             </li>
             <li>
               <a href="#">
-                <i className="fas fa-envelope"> <AiOutlineMail /></i> {/* Ícone para o link */}
+                <i className="fas fa-envelope">
+                  {" "}
+                  <AiOutlineMail />
+                </i>{" "}
+                {/* Ícone para o link */}
                 <span>Contato</span> {/* Descrição do link */}
               </a>
             </li>
@@ -159,19 +183,39 @@ function App() {
         <header className="header">
           <h1>Poke API</h1>
         </header>
-        <Modal isOpen={isModalOpen} onClose={handleCloseModal} pokemon={selectedPokemon} />
+
+        <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        pokemon={selectedPokemon}
+        handleDeletePokemon={handleDeletePokemon}
+        setPokemonList={setPokemonList}
+        pokemonList={pokemonList}
+        updatePokemon={updatePokemon} // Passa a função updatePokemon para o Modal
+      />
+
         <div className="view-mode-selector">
           {/* Select para alternar entre os modos de visualização */}
           <label htmlFor="view-mode-select">Modo de Visualização:</label>
-          <select id="view-mode-select" value={viewMode} onChange={e => setViewMode(e.target.value)}>
+          <select
+            id="view-mode-select"
+            value={viewMode}
+            onChange={(e) => setViewMode(e.target.value)}
+          >
             <option value="cards">Cards</option>
             <option value="list">Lista</option>
           </select>
         </div>
         <div className="pagination-container-right">
           <div className="pagesize-selector-right">
-            <label htmlFor="pagesize-select">Selecione o número de itens por página: </label>
-            <select id="pagesize-select" value={pageSize} onChange={(e) => handlePageSizeChange(e)}>
+            <label htmlFor="pagesize-select">
+              Selecione o número de itens por página:{" "}
+            </label>
+            <select
+              id="pagesize-select"
+              value={pageSize}
+              onChange={(e) => handlePageSizeChange(e)}
+            >
               <option value={5}>5</option>
               <option value={10}>10</option>
               <option value={20}>20</option>
@@ -183,15 +227,19 @@ function App() {
         <div className="pagination-container-right">
           {/* Seletor de tamanho de página */}
         </div>
-        <div className={viewMode === 'cards' ? 'card-container' : 'list-container'}>
+        <div
+          className={viewMode === "cards" ? "card-container" : "list-container"}
+        >
           {/* Renderização condicional baseada no modo de visualização */}
-          {viewMode === 'cards' ? (
-            pokemonList.map(pokemon => <PokemonCard pokemon={pokemon} />)
+          {viewMode === "cards" ? (
+            pokemonList.map((pokemon) => <PokemonCard pokemon={pokemon} />)
           ) : (
             <div className="table-container">
               <table>
                 <PokemonTableHeader />
-                {pokemonList.map(pokemon => <PokemonTableRow key={pokemon.id} pokemon={pokemon} />)}
+                {pokemonList.map((pokemon) => (
+                  <PokemonTableRow key={pokemon.id} pokemon={pokemon} />
+                ))}
               </table>
             </div>
           )}
@@ -200,14 +248,37 @@ function App() {
           {/* Botões de paginação */}
           <div className="pagination-container">
             <div className="pagination-buttons">
-              <button onClick={handleFirstPage} disabled={currentPage === 1}>Primeira</button>
-              <button onClick={() => fetchPokemonList(currentPage - 1, pageSize)} disabled={currentPage === 1}>Anterior</button>
-              <button onClick={() => fetchPokemonList(currentPage + 1, pageSize)} disabled={currentPage === totalPages}>Próxima</button>
-              <button onClick={handleLastPage} disabled={currentPage === totalPages}>Última</button>
+              <button onClick={handleFirstPage} disabled={currentPage === 1}>
+                Primeira
+              </button>
+              <button
+                onClick={() => fetchPokemonList(currentPage - 1, pageSize)}
+                disabled={currentPage === 1}
+              >
+                Anterior
+              </button>
+              <button
+                onClick={() => fetchPokemonList(currentPage + 1, pageSize)}
+                disabled={currentPage === totalPages}
+              >
+                Próxima
+              </button>
+              <button
+                onClick={handleLastPage}
+                disabled={currentPage === totalPages}
+              >
+                Última
+              </button>
             </div>
             <div className="pagesize-selector">
-              <label htmlFor="pagesize-select">Selecione o número de itens por página: </label>
-              <select id="pagesize-select" value={pageSize} onChange={(e) => handlePageSizeChange(e)}>
+              <label htmlFor="pagesize-select">
+                Selecione o número de itens por página:{" "}
+              </label>
+              <select
+                id="pagesize-select"
+                value={pageSize}
+                onChange={(e) => handlePageSizeChange(e)}
+              >
                 <option value={5}>5</option>
                 <option value={10}>10</option>
                 <option value={20}>20</option>
@@ -217,9 +288,7 @@ function App() {
             </div>
           </div>
         </div>
-        <footer className="footer">
-          powered by Gui
-        </footer>
+        <footer className="footer">powered by Gui</footer>
       </div>
     </div>
   );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,172 @@
-import React, { useState } from 'react';
-import './Modal.css'; // Importe o arquivo CSS para estilização do modal
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Modal.css";
 
-function Modal({ isOpen, onClose, pokemon }) {
-  if (!isOpen) return null; // Retorna null se o modal não estiver aberto
+function Modal({
+  isOpen,
+  onClose,
+  pokemon,
+  handleDeletePokemon,
+  setPokemonList,
+  pokemonList,
+  updatePokemon,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedPokemon, setEditedPokemon] = useState({
+    name: "",
+    nationalId: "",
+    firstAttribute: "",
+    secondAttribute: "",
+    megaEvolution: "",
+  });
+
+  const convertToBR = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+  };
+
+  useEffect(() => {
+    setEditedPokemon({ ...pokemon });
+  }, [pokemon]);
+
+  const handleDelete = async () => {
+    await handleDeletePokemon(pokemon.id);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedPokemon({
+      ...pokemon, // Mantém os valores originais do Pokémon
+      updatedAt: new Date().toISOString() // Define a data de atualização como a data atual
+    });
+  };
+
+  const handleSubmitEdit = async () => {
+    try {
+      await axios.put(
+        `http://localhost:8000/pokemon/${pokemon.id}`,
+        editedPokemon
+      );
+      setIsEditing(false);
+      // Atualiza os detalhes do Pokémon no estado local pokemonList
+      const updatedPokemonList = pokemonList.map((p) =>
+        p.id === pokemon.id ? { ...p, ...editedPokemon } : p
+      );
+      setPokemonList(updatedPokemonList);
+      updatePokemon(editedPokemon);
+    } catch (error) {
+      console.error("Erro ao editar Pokémon:", error);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+  };
+
+  if (!isOpen) return null;
 
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="modal-content large-modal"
+        onClick={(e) => e.stopPropagation()}
+      >
         <h2>{pokemon.name}</h2>
         <p>ID: {pokemon.id}</p>
-        <p>firstAttribute: {pokemon.firstAttribute}</p>
-        <p>secondAttribute: {pokemon.secondAttribute}</p>
-        <p>megaEvolution: {pokemon.megaEvolution}</p>
-        <p>Criado em: {pokemon.createdAt}</p>
-        <p>Atualizado em: {pokemon.updatedAt}</p>
-        {/* Adicione mais detalhes do Pokémon conforme necessário */}
-        <button onClick={onClose}>Fechar</button>
+        {isEditing ? (
+          <div className="edit-form">
+            <label>Nome: </label>
+            <br />
+            <input
+              type="text"
+              value={editedPokemon.name}
+              onChange={(e) =>
+                setEditedPokemon({ ...editedPokemon, name: e.target.value })
+              }
+            />
+            <br />
+            <label>National ID: </label>
+            <br />
+            <input
+              type="text"
+              value={editedPokemon.nationalId}
+              onChange={(e) =>
+                setEditedPokemon({
+                  ...editedPokemon,
+                  nationalId: e.target.value,
+                })
+              }
+            />{" "}
+            <br />
+            <label>firstAttribute: </label>
+            <br />
+            <input
+              type="text"
+              value={editedPokemon.firstAttribute}
+              onChange={(e) =>
+                setEditedPokemon({
+                  ...editedPokemon,
+                  firstAttribute: e.target.value,
+                })
+              }
+            />{" "}
+            <br />
+            <label>secondAttribute: </label>
+            <br />
+            <input
+              type="text"
+              value={editedPokemon.secondAttribute}
+              onChange={(e) =>
+                setEditedPokemon({
+                  ...editedPokemon,
+                  secondAttribute: e.target.value,
+                })
+              }
+            />{" "}
+            <br />
+            <label>megaEvolution: </label>
+            <br />
+            <input
+              type="text"
+              value={editedPokemon.megaEvolution}
+              onChange={(e) =>
+                setEditedPokemon({
+                  ...editedPokemon,
+                  megaEvolution: e.target.value,
+                })
+              }
+            />
+            <br />
+            <button onClick={handleSubmitEdit}>Salvar</button>
+            <button onClick={handleCancelEdit}>Cancelar</button>
+          </div>
+        ) : (
+          <>
+            <p>national ID: {pokemon.nationalId}</p>
+            <p>firstAttribute: {pokemon.firstAttribute}</p>
+            <p>secondAttribute: {pokemon.secondAttribute}</p>
+            <p>megaEvolution: {pokemon.megaEvolution}</p>
+          </>
+        )}
+        <p className="small-text">
+          Criado em: {convertToBR(pokemon.createdAt)}
+        </p>
+        <p className="small-text">
+          Atualizado em: {convertToBR(pokemon.updatedAt)}
+        </p>
+        <center>
+          <button onClick={handleEdit} disabled={isEditing}>
+            Editar
+          </button>
+          {"   "}
+          <button onClick={handleDelete} disabled={isEditing}>
+            Excluir
+          </button>
+          {"   "}
+          <button onClick={onClose} disabled={isEditing}>
+            Fechar
+          </button>
+        </center>
       </div>
     </div>
   );
